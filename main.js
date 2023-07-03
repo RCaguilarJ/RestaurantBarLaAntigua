6976208a7c32c186f2b43d7429d1a67bb958a88a
Carlos Aguilar <carlos.aguilar_amk@outlook.com>
2023-07-03 16:52:49 -0600

    arreglo de imagen

--- main.js
+++ main.js
@@ -183,18 +183,6 @@ productList.push({
     "Piña asadarellena de pulpa, camarones y surimi, queso gratinado, salsa diabla, acompañado de arroz y ensalada  ",
 });
 
-// productList.push({
-//   name: "Corte Rib Eye",
-//   price: 210,
-//   image: "logos/corteRibEye.jpeg",
-//   description: "Corte Rib Eye de 250gr  al termino de su elección ",
-// });
-// productList.push({
-//   name: "Corte New York",
-//   price: 215,
-//   image: "/logos/NewYork.jpeg",
-//   description: "Corte New York de 250gr  al termino de su elección ",
-// });
 productList.push({
   name: "Corte T-bone",
   price: 187,
